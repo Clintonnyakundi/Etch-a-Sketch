b95460cb5df4183c6cb5843e869283edb0bca907
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-25 16:01:05 +0300

    Add click to draw functionality

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 let color = "black";
+let click = false;
+
 document.addEventListener("DOMContentLoaded", function() {
     makeGrid();
+
+    let body = document.querySelector("body");
+    body.addEventListener("click", function(e) {
+        if (e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click) {
+                draw.innerHTML = "Let's draw, shall we?";
+            } else {
+                draw.innerHTML = "Oops! Click again to draw";
+            }
+        }
+    })
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function() {
         let size = getSize();
@@ -35,10 +50,12 @@ function getSize() {
 }
 
 function drawGrid() {
-    if (color == "random") {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } else {
-        this.style.backgroundColor = "black";
+    if (click) {
+        if (color == "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = "black";
+        }
     }
 }
 
